d25a7fa272ad4130715ff6dcda5f68256368f0c5
Denis N <service4tv@gmail.com>
2019-07-11 21:18:37 +0300

    Add project files.

---
+++ ISummator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Summator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISummator" in both code and config file together.
+    [ServiceContract]
+    public interface ISummator
+    {
+        [OperationContract]
+        int GetSumm(int a, int b);
+        [OperationContract]
+        int GetDif(int a, int b);
+        [OperationContract]
+        int GetMul(int a, int b);
+        [OperationContract]
+        int GetDiv(int a, int b);
+    }
+}
---
+++ Summator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Summator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Summator" in both code and config file together.
+    public class Summator : ISummator
+    {
+        public int GetDif(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int GetDiv(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int GetMul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int GetSumm(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
